98dfd7e92c6aeaa8efc8f4d8d2787d4b88743052
joni-jones <jonik.lowkick@gmail.com>
2013-10-30 18:01:55 +0200

    Started ukrainian translation

--- language/ukrainian/auth_lang.php
+++ language/ukrainian/auth_lang.php
@@ -9,6 +9,9 @@
 * Author: Daniel Davis
 *         @ourmaninjapan
 *
+* Translation: Ievgen Sentiabov
+*         @joni-jones
+*
 * Location: http://github.com/benedmunds/ion_auth/
 *
 * Created:  03.09.2013
@@ -21,57 +24,57 @@
 $lang['error_csrf'] = 'Форма не пройшла перевірку безпеки.';
 
 // Login
-$lang['login_heading']         = 'Login';
-$lang['login_subheading']      = 'Please login with your email/username and password below.';
-$lang['login_identity_label']  = 'Email/Username:';
-$lang['login_password_label']  = 'Password:';
-$lang['login_remember_label']  = 'Remember Me:';
-$lang['login_submit_btn']      = 'Login';
-$lang['login_forgot_password'] = 'Forgot your password?';
+$lang['login_heading']         = 'Авторизація';
+$lang['login_subheading']      = 'Авторизуйтесь за допомогою вашої електронної пошти/ім\'я користувача та пароля.';
+$lang['login_identity_label']  = 'Електронна пошта/Ім\'я користувача:';
+$lang['login_password_label']  = 'Пароль:';
+$lang['login_remember_label']  = 'Запам\'ятати мене:';
+$lang['login_submit_btn']      = 'Вхід';
+$lang['login_forgot_password'] = 'Втратили пароль?';
 
 // Index
-$lang['index_heading']           = 'Users';
-$lang['index_subheading']        = 'Below is a list of the users.';
-$lang['index_fname_th']          = 'First Name';
-$lang['index_lname_th']          = 'Last Name';
-$lang['index_email_th']          = 'Email';
-$lang['index_groups_th']         = 'Groups';
-$lang['index_status_th']         = 'Status';
-$lang['index_action_th']         = 'Action';
-$lang['index_active_link']       = 'Active';
-$lang['index_inactive_link']     = 'Inactive';
-$lang['index_create_user_link']  = 'Create a new user';
-$lang['index_create_group_link'] = 'Create a new group';
+$lang['index_heading']           = 'Користувачі';
+$lang['index_subheading']        = 'Доступний список користувачів.';
+$lang['index_fname_th']          = 'Ім\'я';
+$lang['index_lname_th']          = 'Прізвище';
+$lang['index_email_th']          = 'Електронна Пошта';
+$lang['index_groups_th']         = 'Групи';
+$lang['index_status_th']         = 'Статус';
+$lang['index_action_th']         = 'Дія';
+$lang['index_active_link']       = 'Активний';
+$lang['index_inactive_link']     = 'Неактивний';
+$lang['index_create_user_link']  = 'Створити нового користувача';
+$lang['index_create_group_link'] = 'Створити нову групу';
 
 // Deactivate User
-$lang['deactivate_heading']                  = 'Deactivate User';
-$lang['deactivate_subheading']               = 'Are you sure you want to deactivate the user \'%s\'';
-$lang['deactivate_confirm_y_label']          = 'Yes:';
-$lang['deactivate_confirm_n_label']          = 'No:';
-$lang['deactivate_submit_btn']               = 'Submit';
-$lang['deactivate_validation_confirm_label'] = 'confirmation';
-$lang['deactivate_validation_user_id_label'] = 'user ID';
+$lang['deactivate_heading']                  = 'Деактивувати Користувача';
+$lang['deactivate_subheading']               = 'Ви впевнені у деактивації користувача \'%s\'';
+$lang['deactivate_confirm_y_label']          = 'Так:';
+$lang['deactivate_confirm_n_label']          = 'Ні:';
+$lang['deactivate_submit_btn']               = 'Відправити';
+$lang['deactivate_validation_confirm_label'] = 'підтвердження';
+$lang['deactivate_validation_user_id_label'] = 'Ідентифікатор користувача';
 
 // Create User
-$lang['create_user_heading']                           = 'Create User';
-$lang['create_user_subheading']                        = 'Please enter the users information below.';
-$lang['create_user_fname_label']                       = 'First Name:';
-$lang['create_user_lname_label']                       = 'Last Name:';
-$lang['create_user_company_label']                     = 'Company Name:';
-$lang['create_user_email_label']                       = 'Email:';
-$lang['create_user_phone_label']                       = 'Phone:';
-$lang['create_user_password_label']                    = 'Password:';
-$lang['create_user_password_confirm_label']            = 'Confirm Password:';
-$lang['create_user_submit_btn']                        = 'Create User';
-$lang['create_user_validation_fname_label']            = 'First Name';
-$lang['create_user_validation_lname_label']            = 'Last Name';
-$lang['create_user_validation_email_label']            = 'Email Address';
-$lang['create_user_validation_phone1_label']           = 'First Part of Phone';
-$lang['create_user_validation_phone2_label']           = 'Second Part of Phone';
-$lang['create_user_validation_phone3_label']           = 'Third Part of Phone';
-$lang['create_user_validation_company_label']          = 'Company Name';
-$lang['create_user_validation_password_label']         = 'Password';
-$lang['create_user_validation_password_confirm_label'] = 'Password Confirmation';
+$lang['create_user_heading']                           = 'Створити Користувача';
+$lang['create_user_subheading']                        = 'Будь-ласка, введіть інформацію про користувача.';
+$lang['create_user_fname_label']                       = 'Ім\'я:';
+$lang['create_user_lname_label']                       = 'Прізвище:';
+$lang['create_user_company_label']                     = 'Компанія:';
+$lang['create_user_email_label']                       = 'Електронна Пошта:';
+$lang['create_user_phone_label']                       = 'Номер Телефону:';
+$lang['create_user_password_label']                    = 'Пароль:';
+$lang['create_user_password_confirm_label']            = 'Підтвердження Пароля:';
+$lang['create_user_submit_btn']                        = 'Створити Користувача';
+$lang['create_user_validation_fname_label']            = 'Ім\'я';
+$lang['create_user_validation_lname_label']            = 'Прізвище';
+$lang['create_user_validation_email_label']            = 'Електронна Адреса';
+$lang['create_user_validation_phone1_label']           = 'Перша Частина Телефону';
+$lang['create_user_validation_phone2_label']           = 'Друга Частина Телефону';
+$lang['create_user_validation_phone3_label']           = 'Третя Частина Телефону';
+$lang['create_user_validation_company_label']          = 'Назва Компанії';
+$lang['create_user_validation_password_label']         = 'Пароль';
+$lang['create_user_validation_password_confirm_label'] = 'Підтвердження Пароля';
 
 // Edit User
 $lang['edit_user_heading']                           = 'Edit User';
